9a7b31eb78f44005147d5a1c3b1ac349077327f8
Hector2523 <heitor.fsilva1000@gmail.com>
2024-02-14 22:24:51 -0300

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,74 +1,26 @@
-var isClose = true;
-let menu = document.querySelector("menu");
-let arrowClose = document.querySelector(".arrowClose");
-let arrowOpen = document.querySelector(".arrowOpen");
-
-function closeMenu() {
-    menu.classList.toggle("open");
-    arrowClose.style.display = "flex";
-    arrowClose.style.opacity = "0";
-    document.querySelector(".nav").style.opacity = "0";
-    isClose = true;
-
-    setTimeout(none, 1000);
-
-    function none() {
-        arrowClose.style.display = "none";
-        arrowOpen.style.display = "inline-flex";
-
-        document.querySelector(".nav").style.display = "none";
-        setTimeout(opacity, 50);
-
-        function opacity() {
-            arrowOpen.style.opacity = "1";
-        }
-
-    }
-}
+var menu = document.getElementById("menu");
+var arrowOpen = document.querySelector("span.arrowOpen");
+var arrowClose = document.querySelector("span.arrowClose");
+var nav = document.getElementById("nav");
 
 function openMenu() {
-    menu.classList.add("open");
-    arrowOpen.style.display = "inline-flex";
+    menu.classList.toggle("enabled");
     arrowOpen.style.opacity = "0";
-    document.querySelector(".nav").style.display = "inline-flex";
-    isClose = false;
-    setTimeout(none, 1000);
-
-    function none() {
+    setTimeout(() => {
         arrowOpen.style.display = "none";
         arrowClose.style.display = "inline-flex";
-
-        setTimeout(opacity, 50);
-
-        function opacity() {
-            arrowClose.style.opacity = "1";
-
-            document.querySelector(".nav").style.opacity = "1";
-        }
-    }
-
+    }, 400)
+    arrowClose.style.opacity = "1";
+    nav.style.opacity = "1";
 }
 
-window.addEventListener("scroll", () => {
-    if (isClose == false) {
-        closeMenu()
-    }
-
-})
-
-//isMarked = false
-
-var home = document.getElementById("home");
-
-function reset() {
-    home.classList.remove("marked");
-}
-
-//isMarked = true
-
-const menuButton = document.querySelector("button.nav");
-
-
-function welcome() {
-    document.querySelector("main").scrollIntoView()
+function closeMenu() {
+    menu.classList.toggle("enabled");
+    arrowClose.style.opacity = "0";
+    nav.style.opacity = "0";
+    setTimeout(() => {
+        arrowClose.style.display = "none";
+        arrowOpen.style.display = "inline-flex";
+        arrowOpen.style.opacity = "1";
+    }, 400)
 }
